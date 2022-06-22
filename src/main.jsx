a55de02902f03a83ebcf93816506ea714f1de472
jhemi-cristina <jhemilycristina@gmail.com>
2022-06-22 15:28:47 -0300

    new project

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Details } from "./pages/Details";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./styles/global";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Details />
+    </ThemeProvider>
+  </React.StrictMode>
+);
