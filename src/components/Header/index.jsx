868afa926e3c23d9be42188db8b7b8960dfe4699
jhemi-cristina <jhemilycristina@gmail.com>
2022-06-23 13:56:05 -0300

    feat: implement component tag

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,24 @@
-import { Container } from "./styles";
+import { RiShutDownLine } from "react-icons/ri";
+import { Container, Logout, Profile } from "./styles";
 
 export function Header() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/jhemi-cristina.png"
+          alt="Foto do usuário"
+        />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Jhemily Cristina</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,15 +1,28 @@
-import { Container } from "./styles";
+import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
     <Container>
       <Header />
-      <h1>Hello</h1>;
-      <Button title="Entrar" loading />
-      <Button title="Sair" />
-      <Button title="Carregar" />
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href="#">http://www.rocketseat.com.br/"</a>
+          </li>
+          <li>
+            <a href="#">http://www.rocketseat.com.br/"</a>
+          </li>
+        </Links>
+      </Section>
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodeJs" />
+      </Section>
+      <Button title="Voltar" />
     </Container>
   );
 }
