b40bae17df3ff3c5ea09f82d14bf369c19f6a231
jhemi-cristina <jhemilycristina@gmail.com>
2022-06-23 12:56:56 -0300

    feat: implement components

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,9 +1,15 @@
 import { Container } from "./styles";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
+      <Header />
       <h1>Hello</h1>;
+      <Button title="Entrar" loading />
+      <Button title="Sair" />
+      <Button title="Carregar" />
     </Container>
   );
 }
